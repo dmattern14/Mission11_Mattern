6d73c53ad846101af0568721993a6e474a77994d
dmattern14 <130599602+dmattern14@users.noreply.github.com>
2025-04-04 23:41:57 -0600

    Commit made from VS Code Azure Static Web Apps

--- backend/Mission11_Mattern.API/Program.cs
+++ backend/Mission11_Mattern.API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.AllowAnyOrigin()
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
--- backend/Mission11_Mattern.API/obj/Debug/net8.0/Mission11_Mattern.API.AssemblyInfo.cs
+++ backend/Mission11_Mattern.API/obj/Debug/net8.0/Mission11_Mattern.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Mattern.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ab42c8b7c2a7cccc98f2191d74405beeab22b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ac088e1d79fb39a0539c40d621e34d75d56e68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Mattern.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Mattern.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
