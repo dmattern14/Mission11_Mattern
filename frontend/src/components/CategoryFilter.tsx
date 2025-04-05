49ac088e1d79fb39a0539c40d621e34d75d56e68
dmattern14 <130599602+dmattern14@users.noreply.github.com>
2025-04-04 23:28:53 -0600

    added route.json

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -7,7 +7,7 @@ function CategoryFilter({selectedCategories, setSelectedCategories}: { selectedC
 
   useEffect(() => {
     const fetchCategories = async () => {
-      const response = await fetch('https://localhost:5000/api/book/GetBookCategories');
+      const response = await fetch('https://bookproject-mattern-backend-b0ejc7c4bje7huc7.eastus-01.azurewebsites.net/api/Book/GetBookCategories');
       const data = await response.json();
       setCategories(data);
     };
